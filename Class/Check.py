10a474b21d34123e3772901ed8ded060ef7c3b12
meisanggou <zhou5315938@163.com>
2015-11-03 08:53:26 +0800

    add check path

--- Class/Check.py
+++ Class/Check.py
@@ -6,29 +6,35 @@ import re
 __author__ = 'ZhouHeng'
 
 
-def check(regex, str, min_len=1, max_len=-1):
-    if min_len > 0 and len(str) < min_len:
+def check(regex, s, min_len=1, max_len=-1):
+    if type(s) != unicode or type(s) !=  str:
         return False
-    if max_len > 0 and len(str) > max_len:
+    if min_len > 0 and len(s) < min_len:
         return False
-    search_result = re.search(regex, str)
+    if max_len > 0 and len(s) > max_len:
+        return False
+    search_result = re.search(regex, s)
     if search_result is not None:
         return False
     return True
 
 
-def check_char_num_underline(str, min_len=1, max_len=-1):
+def check_char_num_underline(s, min_len=1, max_len=-1):
     regex = '[^a-zA-Z0-9_]'
-    return check(regex, str, min_len, max_len)
+    return check(regex, s, min_len, max_len)
 
 
-def check_chinese(str, min_len=1, max_len=-1):
+def check_chinese(s, min_len=1, max_len=-1):
     regex = u'[^\u4e00-\u9fa5]'
-    return check(regex, str, min_len, max_len)
+    return check(regex, s, min_len, max_len)
 
 
-def check_password(str, min_len=1, max_len=-1):
+def check_password(s, min_len=1, max_len=-1):
     regex = 'a-zA-Z0-9.@_'
-    return check(regex, str, min_len, max_len)
+    return check(regex, s, min_len, max_len)
 
+
+def check_path(s, min_len=1, max_len=-1):
+    regex = '[/a-z_]'
+    return check(regex, s, min_len, max_len)
 
