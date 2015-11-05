44dda815ba6ffea66294a7dde9335053dbb77fef
meisanggou <zhou5315938@163.com>
2015-11-05 09:50:24 +0800

    动态显示URL

--- Web/static/t_js/API_HELP/new_api.js
+++ Web/static/t_js/API_HELP/new_api.js
@@ -8,5 +8,31 @@ function select_change(){
     $("#selected_prefix").text(selected_module.attr("title"));
     $("#selected_desc").text(selected_module.attr("about"));
     $("#module_no").val(selected_module.val());
+    show_comp_url();
 }
-$("#api_module").options[0].selected = true;
+function rTrim(str, c){
+    var s_len = str.length;
+    for(var i=s_len-1;i>=0;i--){
+        if(str[i] != c){
+            return str.substr(0, i+1);
+        }
+    }
+    return "";
+}
+function lTrim(str, c){
+    var s_len = str.length;
+    for(var i=0;i<s_len;i++){
+        if(str[i] != c){
+            return str.substr(i, s_len-i);
+        }
+    }
+    return "";
+}
+function show_comp_url(){
+    var selected_module = $("#api_module option:selected");
+    var prefix = rTrim(selected_module.attr("title"), "/");
+    var input_url = lTrim($("#api_url").val(), "/");
+    $("#api_comp_url").text(prefix + "/" + input_url);
+}
+
+
