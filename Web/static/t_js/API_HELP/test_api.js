9c448fa58b5ff8867d3a68658165e3f5975de8ad
meisanggou <zhou5315938@163.com>
2015-12-04 16:52:07 +0800

    实现获得token

--- Web/static/t_js/API_HELP/test_api.js
+++ Web/static/t_js/API_HELP/test_api.js
@@ -3,25 +3,134 @@
  */
 
 function test_api(){
+    update_res("");
     var param_el = $("input[id$='_value']");
-    console.info(param_el);
+    var body_param = new Object();
+    var header_param = new Object();
     for(var i=0;i<param_el.length;i++){
         var el = param_el[i];
-        console.info(el.id);
-        console.info(el.attributes["param_type"].value);
-        console.info(el.value);
+        var id = el.id;
+        var param_key = id.substr(0, id.indexOf("_value"));
+        var param_value = el.value;
+        console.info(param_key);
+        var param_type = el.attributes["param_type"].value;
+        if(param_type == "body"){
+            body_param[param_key] = param_value;
+        }
+        else if(param_type == "header"){
+            header_param[param_key] = "Basic " + base64encode(param_value);
+        }
+        console.info(body_param);
+        console.info(header_param);
     }
     $.ajax({
-        url: "http://127.0.0.1:58000/api/v3/oauth2/token/ping/?callback=?",
-        dataType:'json',
-        method: "GET",
+        url: "http://127.0.0.1:58000/api/v3/oauth2/token/",
+        method: "POST",
+        contentType: "application/json",
+        headers: header_param,
+        data: JSON.stringify(body_param),
         success:function(data){
-            alert(data.message);
+            console.info(data);
+            update_res(JSON.stringify(JSON.parse(data), null, 4));
+            //update_res(JSON.stringify(data, null, 4));
         },
         error:function(xhr){
+            var res = "状态码：" + xhr.status + "\n";
+            res += "返回值：" + xhr.statusText + "";
+            update_res(res);
             console.info(xhr);
-            alert(xhr.statusText + xhr.status);
         }
     });
 }
 
+function update_res(s){
+    $("#res_text").val(s);
+}
+
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+/**
+ * base64编码
+ * @param {Object} str
+ */
+function base64encode(str){
+    var out, i, len;
+    var c1, c2, c3;
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if (i == len) {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+/**
+ * base64解码
+ * @param {Object} str
+ */
+function base64decode(str){
+    var c1, c2, c3, c4;
+    var i, len, out;
+    len = str.length;
+    i = 0;
+    out = "";
+    while (i < len) {
+        /* c1 */
+        do {
+            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        }
+        while (i < len && c1 == -1);
+        if (c1 == -1)
+            break;
+        /* c2 */
+        do {
+            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        }
+        while (i < len && c2 == -1);
+        if (c2 == -1)
+            break;
+        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+        /* c3 */
+        do {
+            c3 = str.charCodeAt(i++) & 0xff;
+            if (c3 == 61)
+                return out;
+            c3 = base64DecodeChars[c3];
+        }
+        while (i < len && c3 == -1);
+        if (c3 == -1)
+            break;
+        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+        /* c4 */
+        do {
+            c4 = str.charCodeAt(i++) & 0xff;
+            if (c4 == 61)
+                return out;
+            c4 = base64DecodeChars[c4];
+        }
+        while (i < len && c4 == -1);
+        if (c4 == -1)
+            break;
+        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+}
