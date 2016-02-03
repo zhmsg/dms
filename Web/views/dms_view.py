d0e6b14c823145393d88bd88e2884cce40fd8833
meisanggou <zhou5315938@163.com>
2016-02-03 16:34:06 +0800

    debug

--- Web/views/dms_view.py
+++ Web/views/dms_view.py
@@ -109,16 +109,13 @@ def login():
 
 @dms_view.route("/password/", methods=["GET"])
 def password_page():
-    try:
-        if current_user.is_authenticated():
-            return render_template("password.html", user_name=current_user.account, url_prefix=url_prefix)
-        elif "change_token" in session and "expires_in" in session and "user_name" in session:
-            expires_in = session["expires_in"]
-            if expires_in > datetime.now():
-                return render_template("password.html", user_name=session["user_name"],
-                                       change_token=session["change_token"], url_prefix=url_prefix)
-    except Exception as e:
-        print(e.args)
+    if current_user.is_authenticated():
+        return render_template("password.html", user_name=current_user.account, url_prefix=url_prefix)
+    elif "change_token" in session and "expires_in" in session and "user_name" in session:
+        expires_in = session["expires_in"]
+        if expires_in > datetime.now():
+            return render_template("password.html", user_name=session["user_name"],
+                                   change_token=session["change_token"], url_prefix=url_prefix)
     return redirect(url_for("dms_view.login_page"))
 
 
