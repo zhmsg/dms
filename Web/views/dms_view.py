af66b76ca5c465fa6be562679ea753d51efcb7c2
meisanggou <zhou5315938@163.com>
2016-02-03 17:06:28 +0800

    debug

--- Web/views/dms_view.py
+++ Web/views/dms_view.py
@@ -114,13 +114,13 @@ def password_page():
         print("current user auth")
         return render_template("password.html", user_name=current_user.account, url_prefix=url_prefix)
     elif "change_token" in session and "expires_in" in session and "user_name" in session:
+        return "update password"
         print("update password")
         expires_in = session["expires_in"]
         if expires_in > datetime.now():
             return render_template("password.html", user_name=session["user_name"],
                                    change_token=session["change_token"], url_prefix=url_prefix)
     print("will login")
-    return "will login"
     return redirect(url_for("dms_view.login_page"))
 
 
