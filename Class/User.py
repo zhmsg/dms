1a8f37b6789e36fd92d6df32b597fcd4f122a192
meisanggou <zhou5315938@163.com>
2016-04-27 14:51:58 +0800

    增加log权限值

--- Class/User.py
+++ Class/User.py
@@ -48,6 +48,7 @@ class UserManager:
             "status_code_look": {"role_desc": u"查看", "role_value": 32768},
             "status_code_new": {"role_desc": u"新建", "role_value": 65536},
             "status_code_del": {"role_desc": u"删除", "role_value": 131072} } }
+        self._log_role_desc = {"module_desc": u"晶云平台日志", "role_list": {"log_look": {"role_desc": u"查看", "role_value": 262144}}}
         self.role_desc = {"data": self._data_role_desc, "api": self._api_role_desc, "table": self._table_role_desc,
                           "right": self._right_role_desc, "user": self._user_role_desc, "bug": self._bug_role_desc,
                           "status_code": self._status_code_role_desc}
