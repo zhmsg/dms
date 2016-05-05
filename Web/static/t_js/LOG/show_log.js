1de0140c33f5cd1f44a9744593ab8f32ffc08832
meisanggou <zhou5315938@163.com>
2016-05-05 14:17:31 +0800

    auto hide look before

--- Web/static/t_js/LOG/show_log.js
+++ Web/static/t_js/LOG/show_log.js
@@ -9,8 +9,14 @@ function update_search_url(){
     var log_level = $("#log_level").val();
     var url_prefix = $("#url_prefix").val();
     var show_before = $("#show_before").val();
-    if ($("#show_before").is(':checked')){
-        request_args += "look_before=1&"
+    if(log_level == "all") {
+        if ($("#show_before").is(':checked')) {
+            request_args += "look_before=1&"
+        }
+        $("#lab_show_before").show();
+    }
+    else{
+        $("#lab_show_before").hide();
     }
     request_args += "log_level=" + log_level + "&";
     request_args += "url_prefix=" + url_prefix + "&";
