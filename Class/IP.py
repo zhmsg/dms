55e1d0f479c727f957cd342c8c63ab6728b737a7
meisanggou <zhou5315938@163.com>
2016-06-14 16:17:47 +0800

    fix bug

--- Class/IP.py
+++ Class/IP.py
@@ -14,7 +14,7 @@ class IPManager:
     def select_info_info(self, ip_value):
         if type(ip_value) != int:
             return False, "Bad ip value"
-        select_sql = "SELECT ip_value_s,ip_value_e,info1,info2 FROM %s WHERE ip_value_s<%s ORDER BY ip_value_s DESC LIMIT 1;" \
+        select_sql = "SELECT ip_value_s,ip_value_e,info1,info2 FROM %s WHERE ip_value_s<=%s ORDER BY ip_value_s DESC LIMIT 1;" \
                      % (self.ip_info, ip_value)
         result = self.db.execute(select_sql)
         if result <= 0:
