3335bb1d18c09d2983decdca0089328c49817dbf
meisanggou <zhou5315938@163.com>
2016-08-23 14:18:53 +0800

    fix bug

--- start_web.sh
+++ start_web.sh
@@ -4,8 +4,8 @@ cd Table
 python develop_table.py
 
 if [ -d "/jy_static" ];then
-    cp -r Web/static2 /jy_static
+    cp -r ../Web/static2 /jy_static
 fi
+
 cd ../Web
-# nohup python msg_web.py 1>> msg_web.log 2>>msg_web.log &
 nohup gunicorn -c gunicorn.conf msg_web:msg_web 1>>msg_web.log 2>>msg_web.log &
