8465bbeb0fec3ef533246bc0c7e766b8b692b71f
meisanggou <zhou5315938@163.com>
2016-09-07 17:04:25 +0800

    release ytj

--- Web/static2/t_js/Release/Release_ytj_Data.js
+++ Web/static2/t_js/Release/Release_ytj_Data.js
@@ -4,15 +4,40 @@
 
 function receive_success(data)
 {
-    console.info(data);
+    if(data.status != 2){
+        sweetAlert(data.message);
+    }
+    else{
+        var secret_key = data.secret_key;
+        release_ytj(secret_key);
+    }
 }
 
 function receive_secret_key(){
+    $("#btn_release_ytj").attr("disabled", "disabled");
     var request_url = "http://local.gene.ac:3285/yitiji/fabu/";
-    my_async_request(request_url, "POST", null, receive_success);
+    my_async_request(request_url, "GET", null, receive_success);
 }
 
+function release_success(data)
+{
+    sweetAlert(data.message);
+}
+
+function release_ytj(secret_key)
+{
+    var service_ip = $("#service_IP").val();
+    var release_style = $("#use_style").val();
+    var request_data = {"secret_key": secret_key, "ip": service_ip, "style": release_style};
+    console.info(request_data);
+    var request_url = "http://local.gene.ac:3285/yitiji/fabu/";
+    my_async_request(request_url, "POST", request_data, release_success)
+}
 
 $(function(){
-    //receive_secret_key();
+    $("#btn_release_ytj").click(receive_secret_key);
+    $("#service_IP").keyup(function()
+    {
+        $("#service_IP").val(format_ip($("#service_IP").val()));
+    });
 });
