9b2d91fcda7bff23ad24e538d37373a2fd0b5daa
meisanggou <zhou5315938@163.com>
2016-09-07 16:36:03 +0800

    修改提示

---
+++ Web/static2/t_js/Release/Release_ytj_Data.js
@@ -0,0 +1,18 @@
+/**
+ * Created by msg on 9/7/16.
+ */
+
+function receive_success(data)
+{
+    console.info(data);
+}
+
+function receive_secret_key(){
+    var request_url = "http://local.gene.ac:3285/yitiji/fabu/";
+    my_async_request(request_url, "POST", null, receive_success);
+}
+
+
+$(function(){
+    receive_secret_key();
+});
