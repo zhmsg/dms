8563c97188d1d0a62ae78af3d10b5d5b4a7c77b0
meisanggou <zhou5315938@163.com>
2016-10-08 17:07:28 +0800

    remove print

--- Tools/MyEmail.py
+++ Tools/MyEmail.py
@@ -51,7 +51,6 @@ class MyEmailManager:
             msg.attach(MIMEText(self.encoded(content, encoding), "html", encoding))
             smtp.sendmail(user, to, msg.as_string())
             smtp.quit()
-            print("send success")
             return True
         except Exception, e:
             error_message = "MyEmailManager send_mail error %s" % str(e.args)
