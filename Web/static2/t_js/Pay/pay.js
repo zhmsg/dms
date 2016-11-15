63e97d9296c96d8aaa36d45f71530e27e24357b9
meisanggou <zhou5315938@163.com>
2016-11-15 09:28:24 +0800

    add pay.js

---
+++ Web/static2/t_js/Pay/pay.js
@@ -0,0 +1,15 @@
+/**
+ * Created by msg on 11/15/16.
+ */
+
+function init_wx(){
+    wx.config({
+        debug:true,
+        appId:"wx6b30be0bc9149832",
+        timestamp:""
+    });
+}
+
+$(function(){
+   alert("success");
+});
