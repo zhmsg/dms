120df48211631433073064bed7120242c7991ffe
meisanggou <zhou5315938@163.com>
2017-02-16 14:13:20 +0800

    去除注释

--- Web/static2/t_js/API_HELP/test_api.js
+++ Web/static2/t_js/API_HELP/test_api.js
@@ -207,9 +207,6 @@ $(function(){
         new_right = true;
     }
     $("#authorization_value").parent().after('<li><input type="checkbox" id="skip_auth"> Skip Auth</li>');
-    $("#authorization_value").val("fuyoulonghui:fuyoulonghui");
-    $("#filter_str_value").val("( SiftClass in ['D',''] or PolyPhen2hdivClass in ['D','P',''] or MutationTasteClass in ['A','D',''] or MetasvmClass in ['D',''] ) and ExonicEffect in ['nonsynonymous SNV','stopgain','stoploss','frameshift insertion','frameshift deletion','frameshift block substitution','nonframeshift insertion','nonframeshift deletion','nonframeshift block substitution','unknown',''] and Region in ['exonic','exonic;splicing','splicing'] and ( ( PopAllFreq <= 0.01 or PopAllFreq == '' ) and ( PopEasFreq <= 0.01 or PopEasFreq == '' ) and ( Esp6500Freq <= 0.01 or Esp6500Freq == '' ) and ( ExacAllFreq <= 0.01 or ExacAllFreq == '' ) and ( ExacEasFreq <= 0.01 or ExacEasFreq == '' ) )");
-    $("#sample_no_value").val("4714");
     $("#Content-Type_value").val("application/json");
 });
 
