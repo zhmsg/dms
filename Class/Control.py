42377cff34c1fbb513d03615052494f74a2f9f68
meisanggou <zhou5315938@163.com>
2017-03-15 10:14:02 +0800

    dyups读取配置获得server

--- Class/Control.py
+++ Class/Control.py
@@ -24,7 +24,7 @@ from PullRequest import PullRequestManager
 from JingDuData import JingDuDataManager
 from DingMsg import DingMsgManager
 from Dyups import DyUpsManager
-from Class import DATE_FORMAT_STR, release_dir, jd_mysql_host, jd_mysql_db
+from Class import DATE_FORMAT_STR, release_dir, jd_mysql_host, jd_mysql_db, dyups_server
 
 __author__ = 'ZhouHeng'
 
@@ -71,7 +71,7 @@ class ControlManager(object):
         self.manger_email = ["budechao@ict.ac.cn", "biozy@ict.ac.cn"]
         self.jy_log = LogManager()
         self.ding_msg = DingMsgManager("a49a7c62e8601123cd417465ff8037cd8410a3572244903fa694e4b7548a917a")
-        self.dyups_man = DyUpsManager("https://local.dyups.gene.ac")
+        self.dyups_man = DyUpsManager(dyups_server)
 
     def check_user_name_exist(self, user_name, role, check_user_name):
         if role & self.role_value["user_new"] <= 0:
