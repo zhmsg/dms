d2ddaf817792d19f45c69afe91b43dba7af96e89
meisanggou <zhou5315938@163.com>
2017-03-20 09:20:27 +0800

    添加ueditor菜单

--- Web/static2/t_js/portal.js
+++ Web/static2/t_js/portal.js
@@ -42,6 +42,7 @@ $(function(){
             $("#div_current_env").append('<a href="/tornado' + current_href + '">' + '体验Tornado' + '</a>');
         }
     }
+    $("#div_main_menu").append('<a href="' + '/editor/">' + 'UEditor' + '</a>');
     $("#div_main_menu").append('<a href="' + $("#password_url_prefix").val() + '/">' + '修改密码' + '</a>');
     $("#div_main_menu").append('<a href="' + $("#exit_url_prefix").val() + '/">' + '退出' + '</a>');
 });
