802d1fdb54f4cf771be1d03f98fd9226b0c1d46b
meisanggou <zhou5315938@163.com>
2017-04-11 11:13:55 +0800

    18:10不再重启IH

--- Web/views/develop_release_view.py
+++ Web/views/develop_release_view.py
@@ -96,4 +96,4 @@ def list_task():
 
 
 dms_job.append({"func": "%s:system_auto_release" % __name__, "trigger": "cron", "id": "release_ih_daily",
-                "day_of_week": "0-4", "hour": "12,18", "minute": "10"})
+                "day_of_week": "0-4", "hour": "12", "minute": "10"})
