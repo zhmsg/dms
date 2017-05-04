677dd6606417bb089c0477c2cce06855aa306d86
meisanggou <zhou5315938@163.com>
2017-05-04 14:42:18 +0800

    修改启动

--- start_web.sh
+++ start_web.sh
@@ -7,7 +7,8 @@ if [ -d "/jy_static/dms_static" ];then
 fi
 
 cd ../Web
-nohup gunicorn -c gunicorn.conf msg_web:msg_web 1>>msg_web.log 2>>msg_web.log &
+# nohup gunicorn -c gunicorn.conf msg_web:msg_web 1>>msg_web.log 2>>msg_web.log &
+gunicorn -b 0.0.0.0:2200 --backlog 2048 -w 2 -k gevent -t 2 -t 3600 -D --access-logfile msg_web_access.log  --error-logfile msg_web_error.log msg_web:msg_web
 
 cd ../Web2
-nohup gunicorn -b 127.0.0.1:2300 tornado_app:ado_app -k tornado 1>>tornado_app.log 2>>tornado_app.log &
+gunicorn -b 127.0.0.1:2300 -k tornado -D --access-logfile tornado_app_access.log -error-logfile tornado_app_error.log tornado_app:ado_app
