fa24b1355ae8b4f424ddcb110923fed43eaf83d0
meisanggou <zhou5315938@163.com>
2017-06-16 17:26:33 +0800

    ih

--- Web/static2/t_js/portal.js
+++ Web/static2/t_js/portal.js
@@ -13,7 +13,7 @@ function bit_and(role1, role2){
 var menu_list = [["api_look", "api_url_prefix", "API文档"], ["status_code_look", "status_url_prefix", "API状态码"],
     ["table_look", "table_url_prefix", "数据表设计"], ["right_look", "right_url_prefix", "晶云平台操作权限"],
     ["param_look", "param_url_prefix", "晶云公共参数"], ["log_query", "log_url_prefix", "晶云平台日志"], ["jd_basic", "jd_url_prefix", "晶读平台数据"],
-    //["release_ih_N", "release_ih_url_prefix", "重启晶云服务"], ["release_ytj_N", "release_ytj_url_prefix", "重启晶云服务"],
+    ["release_ih_N", "release_ih_url_prefix", "重启晶云服务"], ["release_ytj_N", "release_ytj_url_prefix", "重启晶云服务"],
     ["bug_look", "bug_url_prefix", "问题总结"], ["user_new", "register_url_prefix", "新建用户"],
     ["user_new", "authorize_url_prefix", "用户授权"], ["dyups_look", "dyups_url_prefix", "节点管理"]];
 
