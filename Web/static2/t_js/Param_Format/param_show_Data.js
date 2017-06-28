1bd5b5438473d4ba923a66afd7966c88c9138d1d
meisanggou <zhou5315938@163.com>
2017-06-28 16:29:17 +0800

    可搜索

--- Web/static2/t_js/Param_Format/param_show_Data.js
+++ Web/static2/t_js/Param_Format/param_show_Data.js
@@ -3,11 +3,21 @@
  */
 
 function get_success(data){
-    for(var i=0;i<data.data.length;i++){
-        Add_TR(data.data[i]);
+    $("#" + "t_params_info" + " tr").not(":first").remove();
+    for (var i = 0; i < data.length; i++) {
+        Add_TR(data[i]);
     }
 }
 
 $(function() {
-    my_async_request(location.href, "GET", null, get_success);
+    $("#btn_query").click(function () {
+        var query_params = $("#query_params").val();
+        if (query_params.length > 0) {
+            my_async_request2(location.href + "query/?params=" + query_params, "GET", null, get_success);
+        }
+        else {
+            my_async_request2(location.href, "GET", null, get_success);
+        }
+    });
+    $("#btn_query").click();
 });
