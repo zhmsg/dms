eccd4da5f008f9eafb538cf9b2fb0c6cdad2d987
meisanggou <zhou5315938@163.com>
2017-07-04 09:08:56 +0800

    增加自动保存

--- Web/static2/t_js/Article/add.js
+++ Web/static2/t_js/Article/add.js
@@ -10,6 +10,24 @@ function handler(data) {
         $("#article_title").val(data.title);
         ue.setContent(data.content);
     }
+    else {
+        alert1("保存成功");
+    }
+}
+
+function save_article() {
+    var title = $("#article_title").val();
+    var content = ue.getContent();
+    var abstract = ue.getContentTxt().substr(0, 400);
+    var article_no = $("#article_no").val();
+    var method = "POST";
+    var r_data = {"content": content, "abstract": abstract, "title": title};
+    if (article_no.length == 32) {
+        r_data["article_no"] = article_no;
+        method = "PUT";
+        var r_url = location.href;
+        my_async_request2(r_url, method, r_data, handler);
+    }
 }
 
 $(document).ready(function () {
@@ -21,8 +39,13 @@ $(document).ready(function () {
             my_async_request2(r_url, "GET", null, handler);
         }
     });
+    window.setInterval(save_article, 60000);
     $("#btn_save").click(function () {
         var title = $("#article_title").val();
+        if (title.length < 3) {
+            alert1("标题不可少于3个字符");
+            return;
+        }
         var content = ue.getContent();
         var abstract = ue.getContentTxt().substr(0, 400);
         var article_no = $("#article_no").val();
