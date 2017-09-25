da6a4a525490bbe40fdcf68104d9278da0e2fd60
meisanggou <zhou5315938@163.com>
2017-09-25 16:48:48 +0800

    update IPManager

--- Class/IP.py
+++ Class/IP.py
@@ -1,26 +1,35 @@
 #! /usr/bin/env python
 # coding: utf-8
-__author__ = 'ZhouHeng'
 
-from Tools.Mysql_db import DB
+import os
+from JYTools.DB import DB
+from Class import conf_dir
+
+__author__ = 'ZhouHeng'
 
 
 class IPManager(object):
 
     def __init__(self):
-        self.db = DB()
+        self.db = DB(conf_path=os.path.join(conf_dir, "mysql_dms.conf"))
         self.ip_info = "ip_info_s"
+        self.t_group = "ip_group"
 
     def select_info_info(self, ip_value):
         if type(ip_value) != int:
             return False, "Bad ip value"
-        select_sql = "SELECT ip_value_s,ip_value_e,info1,info2 FROM %s WHERE ip_value_s<=%s ORDER BY ip_value_s DESC LIMIT 1;" \
-                     % (self.ip_info, ip_value)
-        result = self.db.execute(select_sql)
-        if result <= 0:
+        cols = ["ip_value_s", "ip_value_e", "info1", "info2"]
+        where_cond = ["ip_value_s<=%s"]
+        where_cond_args = [ip_value]
+        l = self.db.execute_select(self.ip_info, cols=cols, where_cond=where_cond, where_cond_args=where_cond_args,
+                                   order_by=["ip_value_s"], order_desc=True, limit=1, package=False)
+        if l <= 0:
             return True, {"ip": ip_value, "info1": "", "info2": ""}
         s_ip, e_ip, info1, info2 = self.db.fetchone()
         if ip_value > e_ip:
             info1 = ""
             info2 = ""
         return True, {"ip": ip_value, "info1": info1, "info2": info2}
+
+    def select(self, g_name=None):
+        pass
