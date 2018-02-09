2f51185eab1f96a226d5d56bdf26947490fa801f
meisanggou <zhou5315938@163.com>
2018-02-09 17:05:02 +0800

    show reason

--- Web/static2/t_js/Performance/index.js
+++ Web/static2/t_js/Performance/index.js
@@ -8,10 +8,15 @@ function load_keys(data) {
 
     draw_stacked_bar_chart("#svg_total", statistics.columns, statistics.data);
     for(var i=1; i<statistics.columns.length; i++){
+        var add_field = $('<fieldset></fieldset>');
+        var add_legend = $('<legend></legend>');
+        add_legend.text(statistics.columns[i]);
+        add_field.append(add_legend);
         var add_svg = $('<svg width="860" height="500"></svg>');
         var svg_id = "svg_" + i;
         add_svg.attr("id", svg_id);
-        $("#div_svg").append(add_svg);
+        add_field.append(add_svg);
+        $("#div_svg").append(add_field);
         draw_bar_chart("#" + svg_id, statistics.data, "nick_name", statistics.columns[i]);
     }
     var pr_len = pr_items.length;
