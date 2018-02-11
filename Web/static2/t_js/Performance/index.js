5c00f0f6488eefb85a12eaa42c919ce7fdd6369f
meisanggou <zhou5315938@163.com>
2018-02-11 11:34:57 +0800

    折叠

--- Web/static2/t_js/Performance/index.js
+++ Web/static2/t_js/Performance/index.js
@@ -1,7 +1,8 @@
 var all_users = null;
 
-function load_keys(data) {
+function load_performance(data) {
     var current_user = $("#current_user_name").val();
+    var current_user = "wangzhihao";
 
     var pr_items = data["detail"];
     var statistics = data["statistics"];
@@ -24,17 +25,60 @@ function load_keys(data) {
     var keys = ["month", "module_no", "name", "start_time", "end_time", "user_name", "score", "detail_info"];
     for(var i=0; i<pr_len; i++){
         var pr_item = pr_items[i];
-        for(var j=0; j<pr_item.members.length; j++){
-            var prm_item = $.extend({}, pr_item, pr_item.members[j]);
+        var main_tr_id = "tr_" + pr_item["id"];
+        if(pr_item.members.length > 0){
+            var prm_item = $.extend({}, pr_item, pr_item.members[0]);
             prm_item["score"] = prm_item["score"] / 1000;
             prm_item["start_time"] = timestamp_2_date(pr_item["start_time"]);
             prm_item["end_time"] = timestamp_2_date(pr_item["end_time"]);
-            var add_tr = $("<tr></tr>");
+            var main_tr = $("<tr></tr>");
             for(var k=0; k<keys.length; k++){
                 var td = new_td(keys[k], prm_item);
-                add_tr.append(td);
+                main_tr.append(td);
             }
+            if(pr_item.members.length > 1) {
+                var span_user = $("<span></span>");
+                span_user.text(prm_item["user_name"]);
+                var link_more = $('<a class="status_move">[+]</a>');
+                var user_td = main_tr.find("td[name='td_user_name']");
+                user_td.text("");
+                user_td.append(span_user);
+                user_td.append(link_more);
+                main_tr.click(function(){
+                   var tr_id = $(this).attr("id");
+                   var link = $(this).find("a");
+                   if(link.text() == "[+]"){
+                       link.text("[-]");
+                       $('tr[id^="' + tr_id + '_"]').show();
+                   }
+                    else{
+                       link.text("[+]");
+                       $('tr[id^="' + tr_id + '_"]').hide();
+                   }
+                });
+            }
+            $("#t_mine").append(main_tr);
+            main_tr.attr("id", main_tr_id);
+        }
+        for(var j=1;j<pr_item.members.length; j++){
+            var m_item = pr_item.members[j];
+            m_item["score"] = m_item["score"] / 1000;
+            if(m_item["user_name"] == current_user){
+                $("#" + main_tr_id + " td[name='td_user_name'] span").text(m_item["user_name"]);
+                $("#" + main_tr_id + " td[name='td_score']").text(m_item["score"]);
+                m_item = pr_item.members[0];
+                m_item["score"] = m_item["score"] / 1000;
+            }
+            var add_tr = $("<tr></tr>");
+            for(var k=0; k<5;k++){
+                add_tr.append($("<td></td>"));
+            }
+            add_tr.append(new_td("user_name", m_item));
+            add_tr.append(new_td("score", m_item));
+            add_tr.append($("<td></td>"));
+            add_tr.attr("id", main_tr_id + "_" + m_item["user_name"]);
             $("#t_mine").append(add_tr);
+            add_tr.hide();
         }
     }
 }
@@ -46,5 +90,5 @@ $(document).ready(function () {
     if(months == null){
         months = "";
     }
-    my_async_request2(url, "GET", {"months": months}, load_keys)
+    my_async_request2(url, "GET", {"months": months}, load_performance)
 });
