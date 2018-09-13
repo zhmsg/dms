be8915e568d1268c1165655464b3863699fa3762
local10 <local10@gene.ac>
2018-09-13 11:42:54 +0800

    update

--- Web/static2/t_js/API_Status/status_api.js
+++ Web/static2/t_js/API_Status/status_api.js
@@ -12,19 +12,13 @@ function get_module_info_success(data){
     module_info = data.data;
     s_vm.module_info = module_info;
     m_vm.module_info = module_info;
-    for(var key in m_vm.module_info){
-        m_vm.select_module = key;
-        m_vm.change_module();
-        break;
+    if(!(m_vm.select_module in m_vm.module_info)) {
+        for (var key in m_vm.module_info) {
+            m_vm.select_module = key;
+            break;
+        }
     }
-    //$("tr[id^='s_']").each(function() {
-    //    var code = this.id.substr(2, 8);
-    //    var service_id = code.substr(0, 2);
-    //    var service_title = module_info[service_id].title;
-    //    var fun_id = code.substr(2, 2);
-    //    var fun_title = module_info[service_id]["fun_info"][fun_id].title;
-    //    $(this).find("td:eq(1)").text(service_title + "-" + fun_title);
-    //});
+    m_vm.change_module();
 }
 function get_module_info() {
     var request_url = $("#fun_info_url").val();
@@ -34,114 +28,16 @@ function get_module_info() {
 function get_error_type_success(data){
     error_type = data;
     m_vm.error_type = error_type;
-    for(var key in m_vm.error_type){
-        m_vm.select_type = key;
-        m_vm.update_add_desc();
+    if(!(m_vm.select_type in m_vm.error_type)) {
+        for (var key in m_vm.error_type) {
+            m_vm.select_type = key;
+        }
     }
+    m_vm.update_add_desc();
 }
 function get_error_type() {
     var request_url = $("#error_type_url").val();
     my_request2(request_url, "GET", null, get_error_type_success);
-}
-
-function set_service_id(){
-
-    var select_obj = $("#service_id");
-    select_obj.empty();
-    for(var key in module_info){
-        add_option("service_id", key, module_info[key].title, module_info[key].title);
-    }
-    set_fun_id();
-}
-function set_fun_id(){
-    console.info("enter set fun  id");
-    var select_obj = $("#fun_id");
-    select_obj.empty();
-    var service_id = $("#service_id").val();
-    var fun_list = module_info[service_id]["fun_info"];
-    for(var key in fun_list){
-        add_option("fun_id", key, fun_list[key].title, fun_list[key].title);
-    }
-    update_info();
-}
-
-function set_error_type(){
-    var select_obj = $("#type_id");
-    select_obj.empty();
-    for(var key in error_type){
-        add_option("type_id", key, error_type[key].type_title, error_type[key].type_title);
-    }
-}
-
-function fun_update(){
-    var service_id = $("#service_id").val();
-    var fun_id = $("#fun_id").val();
-    var service_desc = '<span class="font-red">' + module_info[service_id].title + '</span>: '  + module_info[service_id].desc;
-    var fun_desc = '<span class="font-red">' + module_info[service_id]["fun_info"][fun_id].title + '</span>: '  + module_info[service_id]["fun_info"][fun_id].desc;
-    $("#service_desc").html(service_desc);
-    $("#fun_desc").html(fun_desc);
-}
-function error_type_update(){
-    var type_id = $("#type_id").val();
-    var desc = '<span class="font-red">' + error_type[type_id].type_title + '</span>: '  + error_type[type_id].type_desc;
-    $("#error_type_desc").html(desc);
-}
-
-function update_info(){
-    var service_id = $("#service_id").val();
-    var service_text = $("#service_id option:selected").text();
-    var fun_id = $("#fun_id").val();
-    var fun_text = $("#fun_id  option:selected").text();
-    var type_id = $("#type_id").val();
-    var type_text = $("#type_id  option:selected").text();
-    var info = "您将新建一个服务模块为 " + '<span class="font-red">' + service_text + '</span>';
-    info += " 功能模块为 " + '<span class="font-red">' + fun_text + '</span>';
-    info += " 错误类型为 " + '<span class="font-red">' + type_text + '</span>';
-    var error_id = $("#error_id").val();
-    var invalid_input = false;
-    if(error_id.length <= 0){
-        error_id = "请填写";
-        invalid_input = true;
-    }
-    else {
-        var error_id_int = parseInt(error_id);
-        if (isNaN(error_id_int)) {
-            error_id = "必须为正整数";
-            invalid_input = true;
-        }
-        else if (error_id_int < 0 || error_id_int >= 100) {
-            error_id = "必须小于100";
-            invalid_input = true;
-        }
-    }
-    info += " 错误状态码为 " + '<span class="font-red">' + error_id + '</span>';
-    var error_desc = $("#error_desc").val();
-    if (error_desc.length <= 0){
-        error_desc = "请填写";
-        invalid_input = true;
-    }
-    if(invalid_input == true){
-        $("#btn_new").attr({"disabled":"disabled"});
-    }
-    else{
-        $("#btn_new").removeAttr("disabled");
-    }
-    //$("#btn_new").disabled = invalid_input;
-    info += " 错误描述为 " + '<span class="font-red">' + error_desc + '</span>';
-    info += " 最终状态码为 " + '<span class="font-red">' + service_id + " " + fun_id + " " + type_id + " " + error_id + '</span>';
-    $("#new_info_show").html(info);
-    fun_update();
-    error_type_update();
-    if($("#show_exist").is(':checked')) {
-        var code = service_id + fun_id + type_id;
-        $("#search_code").val(code);
-        search_type = "start";
-        filter2(code, "start");
-    }
-    else{
-        search_type = "in";
-        filter2($("#search_code").val(), "in");
-    }
 }
 
 
@@ -182,13 +78,9 @@ $(function(){
     $("#conBtn").click(function(){
         var conBtnValue = $("#conBtn").html();
         if(conBtnValue == "单个新建"){
-            set_error_type();
-            set_service_id();
-
             $(".newMode").show();
             $("#conBtn").html("隐藏新建");
             search_type = "start";
-
             load_location_status();
         }
         if(conBtnValue == "隐藏新建"){
@@ -201,29 +93,23 @@ $(function(){
 
 // 存储当前选中的服务模块与 功能模块
 function save_location_status(){
-    var current_select = $("#service_id").val() + "," + $("#fun_id").val();
+    var current_select = m_vm.select_module + "," + m_vm.select_fun + "," + m_vm.select_type;
     localStorage.setItem("dms_api_status_add", current_select);
 }
 
 function load_location_status(){
     var current_select = localStorage.getItem("dms_api_status_add");
+    console.info(current_select);
     if(current_select == null){
         return false;
     }
     var select_ids = current_select.split(",");
     if(select_ids.length >= 2) {
-        var service_id = select_ids[0];
-        var select_options = query_option("service_id", service_id, "value");
-        if(select_options.length > 0){
-            select_options.attr("selected", true)
+        m_vm.select_module = select_ids[0];
+        m_vm.select_fun = select_ids[1];
+        if(select_ids.length >= 3){
+            m_vm.select_type = select_ids[2];
         }
-        set_fun_id();
-        var fun_id = select_ids[1];
-        var select_options = query_option("fun_id", fun_id, "value");
-        if(select_options.length > 0){
-            select_options.attr("selected", true)
-        }
-        update_info();
     }
 
 }
@@ -313,6 +199,9 @@ $(function(){
             update_current_page: function(page_num){
                 this.current_page = page_num;
                 load_page();
+            },
+            copy_code: function(code){
+                copy_text(lTrim(code, '0'));
             }
         }
     });
@@ -337,43 +226,53 @@ $(function(){
             select_fun: "",
             error_type: {},
             select_type: "",
-            add_desc: {"end_code": "00", "desc": ""}
+            add_desc: {"end_code": "00", "status_desc": ""}
         },
         methods: {
             change_module: function () {
                 this.fun_info = this.module_info[this.select_module]["fun_info"];
-                for(var key in this.fun_info){
-                    this.select_fun = key;
-                    break;
+                if(!( this.select_fun in this.fun_info)) {
+                    for (var key in this.fun_info) {
+                        this.select_fun = key;
+                        break;
+                    }
                 }
                 this.update_add_desc();
+                save_location_status();
+            },
+            change_fun: function(){
+                this.update_add_desc();
+                save_location_status();
+            },
+            change_type: function(){
+                this.update_add_desc();
+                save_location_status();
             },
             update_add_desc: function(){
                 this.add_desc["module_title"] = this.module_info[this.select_module]["title"];
+                this.add_desc["module_desc"] = this.module_info[this.select_module]["desc"];
                 this.add_desc["fun_title"] = this.fun_info[this.select_fun]["title"];
+                this.add_desc["fun_desc"] = this.fun_info[this.select_fun]["desc"];
                 this.add_desc["type_title"] = this.error_type[this.select_type]["type_title"];
+                this.add_desc["type_desc"] = this.error_type[this.select_type]["type_desc"];
                 var end_code = this.add_desc["end_code"];
                 if(end_code.length <= 0){
                     end_code = "请填写";
                 }
+                var status_desc_input = this.add_desc["status_desc"];
+                if(status_desc_input.length <= 0){
+                    status_desc_input = "请填写";
+                }
+                this.add_desc["status_desc_input"] = status_desc_input;
                 this.add_desc["end_desc"] = end_code;
                 this.add_desc["show_code"] = this.select_module + " " + this.select_fun + " " + this.select_type + " " + end_code;
             }
         }
     });
+    load_location_status();
     get_module_info();
     get_error_type();
-    if($("#new_info_show").length > 0){
-        $("#service_id").change(set_fun_id);
-        $("#fun_id").change(save_location_status);
-        $(".updateinfo").change(update_info);
-        $("input[id^='error_']").keyup(update_info);
-    }
-    $("td[name='status_code']").click(function(){
-        var current_td = $(this);
-        var code = current_td.text().replace(/[^\d]/g, "");
-        copy_text(lTrim(code, '0'));
-    });
+
     //鼠标移入显示 移出消失 的效果
     $("tr[id^='s_']").hover(
         function(){
